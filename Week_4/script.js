33aa6c0797523d00b7ae65f8bf991458c9527ae6
Eden Brawn <edenbrawn@gmail.com>
2023-03-03 14:51:06 +0000

    added the load function using DOMContentLoaded

--- Week_4/script.js
+++ Week_4/script.js
@@ -6,5 +6,13 @@ function print5() {
     console.log("5");
 }
 
-print5();
-print5();
+
+function myLoadFunction() {
+    var element = document.getElementById('text');
+    element.firstChild.nodeValue = 'Eden Brawn';
+    var pelement = document.getElementById('text2');
+    pelement.firstChild.nodeValue = 'Student';
+}
+
+document.addEventListener('DOMContentLoaded', myLoadFunction); //DOMContentLoaded calls the function until the page has loaded
+
