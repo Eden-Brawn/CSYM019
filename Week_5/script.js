277ef7294d7156566f26b1465d372dbae166988a
Eden Brawn <edenbrawn@gmail.com>
2023-03-10 14:38:51 +0000

    added more css in javascript

--- Week_5/script.js
+++ Week_5/script.js
@@ -3,3 +3,4 @@ document.addEventListener('DOMContentLoaded', pageLoads);
 element.style.color = 'blue';//Alloes you to change CSS
 element.style.width = '100%';
 element.style.height = '50%';
+element.style.fontFamily = 'Verdana, Helvetica, Sans-serif';
