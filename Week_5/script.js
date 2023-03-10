2cadac01e87133624edb14fc37f68188ce4e1387
Eden Brawn <edenbrawn@gmail.com>
2023-03-10 14:51:09 +0000

    changed the opacity of button in excersise 2

--- Week_5/script.js
+++ Week_5/script.js
@@ -5,11 +5,13 @@ function styleChange(){
     //element.style.height = '50%';
     //element.style.fontFamily = 'Verdana, Helvetica, Sans-serif';
     element.style.backgroundColor = 'blue';
+    element.style.color = 'white';
 }
 
 function myLoadFunction(){
     var element = document.getElementById('button');
     element.addEventListener('click', styleChange);
+    element.style.opacity = '0.5';
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
