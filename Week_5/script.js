6f7a178d8483b342472a27c118fe8584e1e6b585
Eden Brawn <edenbrawn@gmail.com>
2023-03-10 14:47:50 +0000

    excersise 2 change button color

--- Week_5/script.js
+++ Week_5/script.js
@@ -1,6 +1,15 @@
-document.addEventListener('DOMContentLoaded', pageLoads);
+function styleChange(){
+    var element = document.getElementById('button');
+    //element.style.color = 'blue';//Alloes you to change CSS
+    //element.style.width = '100%';
+    //element.style.height = '50%';
+    //element.style.fontFamily = 'Verdana, Helvetica, Sans-serif';
+    element.style.backgroundColor = 'blue';
+}
 
-element.style.color = 'blue';//Alloes you to change CSS
-element.style.width = '100%';
-element.style.height = '50%';
-element.style.fontFamily = 'Verdana, Helvetica, Sans-serif';
+function myLoadFunction(){
+    var element = document.getElementById('button');
+    element.addEventListener('click', styleChange);
+}
+
+document.addEventListener('DOMContentLoaded', myLoadFunction);
