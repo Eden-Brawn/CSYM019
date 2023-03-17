a04ce33a490bdd776956bd68b61a62575bd3db26
Eden Brawn <edenbrawn@gmail.com>
2023-03-17 14:52:23 +0000

    using JSON

---
+++ Week_6/script.js
@@ -0,0 +1,11 @@
+var module = {//using JSON
+    "course": "msc computing",
+    "code": "CSYM019",
+    "tutor": {
+        "firstname": "tom",
+        "lastname": "dave"
+    }
+}
+
+alert(module.code);
+alert(module.tutor.firstname);
