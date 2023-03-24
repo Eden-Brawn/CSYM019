c5cd82139bf0e29bd9b67e25a85c0be9741b4fb8
Eden Brawn <edenbrawn@gmail.com>
2023-03-24 14:29:50 +0000

    example code

--- Week_7/script.js
+++ Week_7/script.js
@@ -1,3 +1,16 @@
+//Grabs the data that will be used
 var stars = [135850, 52122, 148825, 16939, 9763];
 var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
 
+var ctx = document.getElementById('myChart');//Gets the canvas area from html
+
+var myChart = new Chart(ctx, {//Makes the chart
+  type: 'bar',
+  data: {
+     labels: frameworks,
+     datasets: [{
+         label: 'Github Stars',
+         data: stars
+     }]
+  },
+ })
