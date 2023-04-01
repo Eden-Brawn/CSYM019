a04673474a4240da0f3a449977b260a0f8259082
Eden Brawn <edenbrawn@gmail.com>
2023-04-01 14:48:44 +0100

    function to show contents if request is complete

--- Week_8/webserver/src/ajaxjson.js
+++ Week_8/webserver/src/ajaxjson.js
@@ -1,6 +1,6 @@
 window.onload = makeAjaxRequest;
 
-function makeAjaxRequest{
+function makeAjaxRequest(){
     if (window.XMLHttpRequest){
         xhr = new XMLHttpRequest();
     }else{
@@ -15,5 +15,15 @@ function makeAjaxRequest{
         xhr.onreadstatechange = showContents;
     }else {
         document.getElementById("updatemessage").innerHTML = "Could not perform stated Request";
+    }
+}
+
+function showContents(){
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200){
+
+        }else{
+            
+        }
     }
 }
