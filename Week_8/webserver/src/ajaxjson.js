5d29f4ff43ed9a7522b5a431f8147a306102ca5f
Eden Brawn <edenbrawn@gmail.com>
2023-04-01 14:29:09 +0100

    filled function to request based on browser type

--- Week_8/webserver/src/ajaxjson.js
+++ Week_8/webserver/src/ajaxjson.js
@@ -1,5 +1,11 @@
 window.onload = makeAjaxRequest;
 
 function makeAjaxRequest{
-    
+    if (window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+    }else{
+        if (window.ActiveXObject){
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
 }
